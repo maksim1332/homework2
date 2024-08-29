0c03b43e9ee3ef6d4f4967940118a4e992ada778
USER <mas9n9-mas9n91332@yandex.ru>
2024-08-30 00:50:25 +0500

    homework2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }//
+        {//task 1
+//            var dog = 8.0;
+//            var cat = 3.6;
+//            var paper = 763789;
+//            System.out.println("dog,"+"cat,"+"paper.");
+        }
+        {//task 2
+            var dog = 8.0;
+            var cat = 3.6;
+            var paper = 763789;
+            dog = dog + 4;
+            cat = cat + 4;
+            paper = paper + 4;
+            System.out.println("dog-"+dog);
+            System.out.println("cat-"+cat);
+            System.out.println("paper-"+paper);
+        }
+    }
 }
