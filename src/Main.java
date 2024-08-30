ef58a23993d324bcb9d86944e0528ad7c91b3a74
USER <mas9n9-mas9n91332@yandex.ru>
2024-08-30 13:58:10 +0500

    homework2

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,58 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        {//task 1
-//            var dog = 8.0;
-//            var cat = 3.6;
-//            var paper = 763789;
-//            System.out.println("dog,"+"cat,"+"paper.");
+        {                                       //task 1
+            var dog = 8.0;
+            var cat = 3.6;
+            var paper = 763789;
+            System.out.println("dog " + "cat " + "paper");
+                                                //task 2
+            dog = dog + 4;
+            cat = cat + 4;
+            paper = paper + 4;
+            System.out.println("dog-" + dog);
+            System.out.println("cat-" + cat);
+            System.out.println("paper-" + paper);
+
+                                                //task 3
+            dog = dog - 3.5;
+            cat = cat - 1.6;
+            paper = paper - 7639;
+            System.out.println("dog "+ dog);
+            System.out.println("cat "+ cat);
+            System.out.println("paper "+ paper);
         }
-        {//task 2
-//            var dog = 8.0;
-//            var cat = 3.6;
-//            var paper = 763789;
-//            dog = dog + 4;
-//            cat = cat + 4;
-//            paper = paper + 4;
-//            System.out.println("dog-"+dog);
-//            System.out.println("cat-"+cat);
-//            System.out.println("paper-"+paper);
+        {                                       //task 4
+            var friend = 19;
+            System.out.println(friend);
+            friend = friend + 2;
+            System.out.println(friend);
+            friend = friend / 7;
+            System.out.println(friend);
         }
-        {//task 3
-//            var  dog = 8.0 ;
-//            var  cat = 3.6 ;
-//            var paper = 763789 ;
-//            dog = dog - 3,5 ;
-//            cat = cat - 1,6 ;
-//            бумага = бумага - 7639 ;
-//            System.out.println("dog-"+dog);
-//            System.out.println("cat-"+cat);
-//            System.out.println("paper-"+paper);
+        {                                       //task 5
+             var frog = 3.5;
+            System.out.println(frog);
+            frog = frog * 10;
+            System.out.println(frog);
+            frog = frog / 3.5;
+            System.out.println(frog);
         }
-        {//task 4
-//            var friend = 19;
-//            System.out.println(friend);
-//            friend = friend + 2;
-//            System.out.println(friend);
-//            friend = friend / 7;
-//            System.out.println(friend);
+        {                                       //task 6
+            var boxerOne = 78.2;
+            var boxerTwo = 82.7;
+            var totalWeight = boxerOne + boxerTwo;
+            System.out.println(totalWeight);
+            var difference = boxerTwo-boxerOne;
+            System.out.println(difference);
         }
-        {//task 5
-//            var frog = 3.5;
-//            System.out.println(frog);
-//            frog = frog * 10;
-//            System.out.println(frog);
-//            frog = frog / 3.5;
-//            System.out.println(frog);
+        {                                        //task 7
+            var boxerOne = 78.2;
+            var boxerTwo = 82.7;
+            var remains = boxerTwo - boxerOne ;
+            System.out.println(remains);
         }
-        {//task 6
-//            var boxerOne = 78.2;
-//            var boxerTwo = 82.7;
-//            var totalWeight = boxerOne + boxerTwo;
-//            System.out.println(totalWeight);
-//            var difference = boxerTwo-boxerOne;
-//            System.out.println(difference);
-        }
-        {//task 7
-//            var boxerOne = 78.2;
-//            var boxerTwo = 82.7;
-//            var remains = boxerTwo - boxerOne ;
-//            System.out.println(remains);
-        }
-        {//task 8
+        {                                        //task 8
             var timeWorked = 640;
             var change = 8;
             var staff = timeWorked / change;
